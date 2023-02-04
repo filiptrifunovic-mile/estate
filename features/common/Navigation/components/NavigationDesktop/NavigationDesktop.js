cc3faa7926c698176818ee807bee846f7c294188
unknown <filip.trifunovic23@gmail.com>
2023-02-04 13:00:38 +0100

    navigation desktop ui

--- features/common/Navigation/components/NavigationDesktop/NavigationDesktop.js
+++ features/common/Navigation/components/NavigationDesktop/NavigationDesktop.js
@@ -1,7 +1,53 @@
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import Link from "next/link";
 import React from "react";
+import { HiHomeModern } from "react-icons/hi2";
+import { navigationLinks } from "../../navigationConsts";
 
 const NavigationDesktop = () => {
-  return <div>NavigationDesktop</div>;
+  return (
+    <Box
+      color="blue.600"
+      py="2rem"
+      backgroundColor="white"
+      display={{ base: "none", md: "block" }}
+    >
+      <Box maxWidth="1280px" margin="0 auto">
+        <Flex alignItems="center" justifyContent="space-between">
+          <Link href="/">
+            <Box display="flex" alignItems="center" gap="2">
+              <HiHomeModern size="30" />
+              <Text fontSize="2xl" fontWeight="black">
+                ESTATE
+              </Text>
+            </Box>
+          </Link>
+          {navigationLinks.map((item, index) => (
+            <NavigationLink key={index} {...item} />
+          ))}
+          <Button
+            padding="1.5rem"
+            colorScheme="twitter"
+            fontSize="0.8rem"
+            fontWeight="medium"
+          >
+            CREATE LISTING
+          </Button>
+        </Flex>
+      </Box>
+    </Box>
+  );
 };
 
 export default NavigationDesktop;
+
+const NavigationLink = ({ title, link, icon }) => {
+  return (
+    <Link href={link}>
+      <Flex alignItems="center" gap="0.5rem">
+        {icon}
+        {title}
+      </Flex>
+    </Link>
+  );
+};
---
+++ features/common/Navigation/navigationConsts.js
@@ -0,0 +1,17 @@
+import { HiPhoneArrowUpRight } from "react-icons/hi2";
+
+export const navigationLinks = [
+  {
+    title: "Properties",
+    link: "/properties",
+  },
+  {
+    title: "Contact",
+    link: "/contact",
+  },
+  {
+    title: "(+381) 75 65 458",
+    link: "tel:(+381) 75 65 458",
+    icon: <HiPhoneArrowUpRight />,
+  },
+];
