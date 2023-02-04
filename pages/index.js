0e8c634c2fcad53531398d1987c09b477cd1eebb
unknown <filip.trifunovic23@gmail.com>
2023-02-04 13:35:22 +0100

    footer component ui

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Footer from "@/features/common/Footer";
 import Navigation from "@/features/common/Navigation";
 
 export default function Home() {
   return (
     <>
       <Navigation />
+      <Footer />
     </>
   );
 }
