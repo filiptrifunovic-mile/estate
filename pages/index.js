0786dd6c2b8887fa12b129bad96a8e01e812118a
unknown <filip.trifunovic23@gmail.com>
2023-02-06 13:30:53 +0100

    meet the team and agent card components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import FeaturedProperties from "@/features/Home/components/FeaturedProperties";
 import HeroBanner from "@/features/Home/components/HeroBanner";
+import MeetTheTeam from "@/features/Home/components/MeetTheTeam";
 import DefaultLayout from "@/features/Layouts/DefaultLayout";
 
 export default function Home({ featuredProperties }) {
@@ -7,6 +8,7 @@ export default function Home({ featuredProperties }) {
     <DefaultLayout>
       <HeroBanner />
       <FeaturedProperties featuredProperties={featuredProperties} />
+      <MeetTheTeam />
     </DefaultLayout>
   );
 }
@@ -14,6 +16,6 @@ export default function Home({ featuredProperties }) {
 export async function getStaticProps() {
   const { hits } = require("@/features/data/properties");
   return {
-    props: { featuredProperties: hits },
+    props: { featuredProperties: hits.slice(0, 5) },
   };
 }
